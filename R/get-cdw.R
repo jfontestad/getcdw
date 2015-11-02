ec33f80498fadb333cd72465831e31406357ac30
tarakc02 <tarakc02@users.noreply.github.com>
2015-11-02 15:59:54 -0800

    initial commit

---
+++ R/get-cdw.R
@@ -0,0 +1,37 @@
+#' Get data from the CDW
+#'
+#' @param query A SQL query, as a string of plain-text.
+#' @param dsn The name of the connection, as it appears in your \code{tnsnames}
+#' @param uid Your username
+#' @param pwd Your password
+#' @param stringsAsFactors TRUE/FALSE. Whether strings should be returned as factors. FALSE by default
+#' @param ... Other arguments passed on to \code{sqlQuery} in package \code{RODBC}
+#'
+#' @details Returns a data.frame (unless)
+#' @importFrom assertthat assert_that is.string
+#' @import RODBC
+#' @export
+get_cdw <- function(query, dsn = "CDW2", uid, pwd,
+                    stringsAsFactors = FALSE, ...) {
+
+    # load package
+    # require(RODBC, quietly=TRUE)
+
+    # name the connection,
+    # you can have multiple connections open (why?)
+    ch <- RODBC::odbcConnect(dsn = dsn, uid = uid, pwd = pwd)
+    on.exit(close(ch))
+
+    #run query
+    outp <- RODBC::sqlQuery(ch, query,
+                            stringsAsFactors = stringsAsFactors, ...)
+
+    if (!is.data.frame(outp)) {
+        errmsg <- regexpr("ORA-[0-9]+:.*$", outp)
+        err <- regmatches(outp, errmsg)
+        stop(err)
+    }
+
+    names(outp) <- tolower(names(outp))
+    dplyr::tbl_df(outp)
+}
